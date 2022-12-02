b77080dab919b517873ee32973990e9ef778fe62
shane murnion <shaneinsweden@gmail.com>
2022-12-02 21:20:45 +0000

    background listener

--- background.js
+++ background.js
@@ -3,6 +3,7 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
       const queryParameters = tab.url.split("?")[1];
       const urlParameters = new URLSearchParams(queryParameters);
   
+      console.log(urlParameters);
       chrome.tabs.sendMessage(tabId, {
         type: "NEW",
         videoId: urlParameters.get("v"),
