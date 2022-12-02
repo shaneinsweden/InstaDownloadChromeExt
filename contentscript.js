4e7e5d4eaf9e577d8d5be7e13709f26e668bdf4b
shane murnion <shaneinsweden@gmail.com>
2022-12-02 21:29:16 +0000

    fixed contentscript bug

--- contentscript.js
+++ contentscript.js
@@ -2,11 +2,13 @@
     let youtubeLeftControls, youtubePlayer;
     let currentVideo = "";
 
-    chrome.runtime.onMessage.addListener(obj, sender, response) => {
-        const {type, value, videoId} = obj;
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const { type, value, videoId } = obj;
 
-        if (type === "NEW")
-        currentVideo = videoId;
-        newVideoLoaded();
-    }
-})();
+        if (type === "NEW") {
+            currentVideo = videoId;
+            newVideoLoaded();
+        }
+    });
+    
+});
