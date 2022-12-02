f31f6f42087f8d34bcfa671d5ae9730b6d6e5380
shane murnion <shaneinsweden@gmail.com>
2022-12-02 21:26:49 +0000

    added hello world message

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if (tab.url && tab.url.includes("youtube.com/watch")) {
       const queryParameters = tab.url.split("?")[1];
       const urlParameters = new URLSearchParams(queryParameters);
-  
+      console.log("hello shane");
       console.log(urlParameters);
       chrome.tabs.sendMessage(tabId, {
         type: "NEW",
